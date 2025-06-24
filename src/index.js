a03e66ef8c44d91a2c64462424e757b0612ddb3f
Mr-Dan-kibet <dan.rotich@student.moringaschool.com>
2025-06-24 15:26:01 +0300

    adds date selection and appropriate styling

---
+++ src/index.js
@@ -0,0 +1,5 @@
+document.addEventListener("DOMContentLoaded", () => {
+  const bookingForm = document.getElementById("Booking-form");
+  const seatButtons = document.querySelectorAll(".seat");
+  const totalDisplay = document.getElementById("bookingTotal");
+});
